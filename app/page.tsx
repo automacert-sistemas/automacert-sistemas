8c12d6a29dcf915e8269ffd097c4a633168c990e
VictorJGB <victorgb.dev@gmail.com>
2025-05-20 17:06:25 -0300

    feat: Finalizando hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Header from "@/components/header";
+import HeroSection from "@/components/sections/hero-section";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen grid-rows-auto">
       <Header />
-      <h1><strong className="text-primary">Automacert</strong> Sistemas</h1>
+
+      {/* Sections */}
+      <main className="min-w-full min-h-screen">
+        <HeroSection />
+      </main>
+
+      <footer className="w-full max-w-container text-center">footer</footer>
     </div>
   );
 }
---
+++ components/sections/hero-section.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image"
+
+// assets
+import Gdoor from '@/public/hero-section/gdoor.png'
+import Hero from '@/public/hero-section/hero.png'
+import Sti3 from '@/public/hero-section/sti3.png'
+
+export default function HeroSection() {
+  return (
+    <section className='min-w-full grid place-items-center'>
+      {/* content */}
+      <div className="min-h-full w-full max-w-container flex items-center justify-center py-9 px-6 gap-20">
+        <div className="flex flex-col items-center lg:items-start justify-center gap-3 lg:basis-1/2">
+          <h1 className="text-5xl lg:text-6xl leading-14 lg:leading-16 text-primary font-bold">Soluções inteligentes para impulsionar o seu negócio</h1>
+          <h4 className="text-lg lg:text-lg font-semibold leading-7 text-slate-500">Automatize processos, reduza erros e leve seu negócio pro próximo nível com nossas soluções completas e seguras.</h4>
+
+          {/* logos */}
+          <div className="flex w-full items-center justify-center lg:justify-start px-6 gap-5">
+            <Image
+              src={Gdoor}
+              alt=""
+              className="w-32"
+            />
+
+            <Image
+              src={Sti3}
+              alt=""
+              className="w-32"
+            />
+          </div>
+        </div>
+
+        {/* image */}
+        <div className="hidden lg:flex basis-1/2">
+          <Image
+            src={Hero}
+            alt=""
+            className="w-full object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
