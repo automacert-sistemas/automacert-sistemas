456c96e8becad2f20ad3d3d75476982d2bfd7d5c
VictorJGB <victorgb.dev@gmail.com>
2025-05-22 11:39:12 -0300

    feat: Mudanças nos links

--- data/nav-links.ts
+++ data/nav-links.ts
@@ -3,5 +3,5 @@ import type Links from "@/interfaces/links";
 export const navLinks: Links[] = [
   { label: 'Serviços', id: '#services', iconName: 'wrench' },
   { label: 'Soluções', id: '#solutions', iconName: 'box' },
-  { label: 'Certificado Digital', id: '#certificate', iconName: 'file-badge' },
+  { label: 'Certificado Digital', id: '#certificates', iconName: 'file-badge' },
 ]
