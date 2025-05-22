eb776bb900a28cbe827cf4342d5930423347c887
VictorJGB <victorgb.dev@gmail.com>
2025-05-22 08:59:04 -0300

    style: Finalizando responsividade da sessão de soluções

--- components/sections/solutions/index.tsx
+++ components/sections/solutions/index.tsx
@@ -37,28 +37,33 @@ export default function SolutionsSection() {
         </div>
 
         {/* main */}
-        <div className="flex items-center justify-center size-full py-8 gap-16">
+        <div className="flex flex-col md:flex-row items-center justify-center size-full py-8 gap-16">
           {/* images */}
-          <div className="w-full lg:w-1/2 h-full flex lg:flex-col items-center justify-center gap-10 lg:gap-16 bg-slate-100 rounded-2xl">
+          <div className="w-full lg:w-1/2 h-full flex md:flex-col items-center justify-center p-4 lg:p-0 gap-10 lg:gap-16 bg-slate-100 rounded-2xl shadow">
           {solutions.map(({image, solution}, index) => (
-            <SolutionCard key={index} image={image} onClick={() => handleClick(solution)} isActive={solution === selectedSolution}/>
+            <SolutionCard 
+              key={index} 
+              image={image} 
+              onClick={() => handleClick(solution)} 
+              isActive={solution === selectedSolution}
+            />
           ))}
           </div>
 
           {/* infos */}
-          <div className="w-full lg:w-1/2 h-full flex flex-col items-center justify-start lg:items-start gap-2.5">
-            <h2 className="text-2xl lg:text-3xl text-primary font-semibold transition-all">{solutionData.title}</h2>
-            <p className="text-lg text-slate-700 transition-all">{solutionData.description}</p>
+          <div className="w-full lg:w-1/2 h-full flex flex-col items-center justify-start md:items-start gap-2.5">
+            <h2 className="text-2xl min-md:max-lg:text-xl lg:text-3xl text-primary font-semibold transition-all">{solutionData.title}</h2>
+            <p className="min-md:max-lg:text-base text-lg text-slate-700 transition-all">{solutionData.description}</p>
             <div className="flex flex-col w-full py-2 items-start justify-center gap-3 transition-all">
               {solutionData.benefits.map((benefit, index) => (
-                <div key={index} className="flex items-center justify-center gap-2.5">
-                  <BadgeCheck className="size-6 text-primary"/>
-                  <span className="text-base font-bold text-primary">{benefit}</span>
+                <div key={index} className="flex items-center justify-center min-md:max-lg:gap-2 gap-2.5">
+                  <BadgeCheck className="min-md:max-lg:size-5 size-6 text-primary"/>
+                  <span className="min-md:max-lg:text-sm text-base font-bold text-primary">{benefit}</span>
                 </div>
               ))}
             </div>
 
-            <Button className="text-lg cursor-pointer m-auto shadow" asChild>
+            <Button className="text-lg cursor-pointer mt-4 md:m-auto shadow" asChild>
               <Link href={solutionData.href} target="_blank">
                 Adquirir solução
               </Link>
