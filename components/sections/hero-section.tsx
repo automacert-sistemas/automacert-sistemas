e817d37caec2db608f160ced5fb33e38092098c0
VictorJGB <victorgb.dev@gmail.com>
2025-05-30 14:19:46 -0300

    feat: Adicionando animações ao hero-section

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -12,11 +12,11 @@ export default function HeroSection() {
       {/* content */}
       <div className="min-h-full w-full max-w-container grid grid-cols-1 lg:grid-cols-2 grid-rows-1 items-center justify-center gap-20">
         <div className="flex flex-col items-center justify-center lg:items-start gap-3 lg:basis-1/2">
-          <h1 className="text-5xl xl:text-6xl leading-14 xl:leading-16 text-center lg:text-start text-primary font-bold">Soluções inteligentes para impulsionar o seu negócio</h1>
-          <h4 className="text-lg lg:text-lg font-semibold leading-7 text-slate-500 text-center lg:text-start">Automatize processos, reduza erros e leve seu negócio pro próximo nível com nossas soluções completas e seguras.</h4>
+          <h1 className="text-5xl xl:text-6xl leading-14 xl:leading-16 text-center lg:text-start text-primary font-bold  motion-preset-slide-up lg:motion-preset-slide-right motion-duration-2000">Soluções inteligentes para impulsionar o seu negócio</h1>
+          <h4 className="text-lg lg:text-lg font-semibold leading-7 text-slate-500 text-center lg:text-start motion-preset-slide-up lg:motion-preset-slide-right motion-duration-2000">Automatize processos, reduza erros e leve seu negócio pro próximo nível com nossas soluções completas e seguras.</h4>
 
           {/* logos */}
-          <div className="grid grid-cols-3 grid-rows-1 items-center justify-center lg:justify-start max-md:px-6 mt-5 gap-5">
+          <div className="grid grid-cols-3 grid-rows-1 items-center justify-center lg:justify-start max-md:px-6 mt-5 gap-5 motion-preset-slide-up lg:motion-preset-slide-right motion-duration-2000">
             <Image
               src={Gdoor}
               alt=""
@@ -35,7 +35,7 @@ export default function HeroSection() {
         </div>
 
         {/* image */}
-        <div className="hidden lg:flex basis-1/2">
+        <div className="hidden lg:flex basis-1/2 motion-preset-slide-up motion-duration-2000">
           <Image
             src={Hero}
             alt=""
