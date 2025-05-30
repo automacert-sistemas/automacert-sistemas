7ef4a1e7ceeae30f4486d62b240ff3e9f881d930
VictorJGB <victorgb.dev@gmail.com>
2025-05-30 17:27:20 -0300

    feat: Adicionando animações a sessão de sobre nós

--- components/sections/about-us/index.tsx
+++ components/sections/about-us/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,10 +11,13 @@ import StatsCard from "./stats-card";
 import { GoogleMaps, Instagram, Whatsapp } from "@/components/icons/custom-icons";
 
 // assets
+import useInView from "@/hooks/useInView";
 import Automacert from '@/public/about-us-section/automacert.png';
 
 
 export default function AboutUsSection() {
+  const { ref, isInView } = useInView({ threshold: 0.3 })
+
   return (
     <section className="min-w-full min-h-full flex items-center justify-center p-section py-6 bg-slate-50 scroll-mt-20" id="aboutUs">
       {/* content */}
@@ -21,11 +26,19 @@ export default function AboutUsSection() {
         <div className="size-full xl:w-1/2 grid grid-cols-1 grid-rows-[auto_1fr_auto_auto] sm:grid-rows-[auto_1fr_auto_1fr] xl:grid-rows-3 items-center justify-start xl:items-start xl:py-6 max-xl:gap-8">
           {/* header */}
           <div className="flex flex-col items-start justify-center text-center lg:text-start gap-5 lg:gap-8">
-            <h2 className="w-full text-2xl lg:text-3xl leading-8 md:leading-9 font-semibold text-primary">SOBRE NÓS</h2>
-            <h1 className="w-full text-4xl lg:text-6xl leading-14 lg:leading-16 font-bold text-primary">Conheça mais sobre nossa empresa</h1>
+            <h2
+              className="w-full text-2xl lg:text-3xl leading-8 md:leading-9 font-semibold text-primary motion-preset-slide-up lg:motion-preset-slide-right motion-duration-1000"
+            >
+              SOBRE NÓS
+            </h2>
+            <h1
+              className="w-full text-4xl lg:text-6xl leading-14 lg:leading-16 font-bold text-primary motion-preset-slide-up lg:motion-preset-slide-right motion-duration-1500"
+            >
+              Conheça mais sobre nossa empresa
+            </h1>
           </div>
 
-          <p className="text-center lg:text-justify text-base sm:text-lg text-slate-500 leading-7 w-full">
+          <p className="text-center lg:text-justify text-base sm:text-lg text-slate-500 leading-7 w-full motion-preset-slide-up lg:motion-preset-slide-right motion-duration-[2.2s]">
             Na Automacert Sistemas, somos especialistas em soluções para transformar a gestão do seu negócio.
             Atuamos com a revenda de softwares de gestão comercial, oferecendo ferramentas robustas, intuitivas e eficientes para otimizar processos e aumentar a produtividade.
             Além disso, somos referência em certificação digital, garantindo segurança e agilidade nas transações eletrônicas da sua empresa.
@@ -37,11 +50,11 @@ export default function AboutUsSection() {
 
           {/* stats card */}
           <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 items-center justify-center gap-8 mt-8">
-            <StatsCard title="+ 3000" description="Clientes satisfeitos" />
-            <StatsCard title="+ 10" description="Anos de experiência" />
+            <StatsCard title="+ 3000" description="Clientes satisfeitos" className="motion-preset-expand motion-duration-1000 lg:motion-duration-1500" />
+            <StatsCard title="+ 10" description="Anos de experiência" className="motion-preset-expand motion-duration-1500 lg:motion-duration-[1.8s]" />
           </div>
 
-          <div className="xl:hidden flex flex-col items-center justify-center gap-4">
+          <div className="xl:hidden flex flex-col items-center justify-center gap-4 motion-preset-slide-up motion-duration-1500">
             <h4 className="text-lg font-semibold text-primary my-3">Saiba onde nos encontrar</h4>
             <div className="flex flex-col md:flex-row items-center justify-center gap-6">
               <Button variant={'whatsapp'} className="sm:text-xl sm:py-6 max-sm:w-full" asChild>
@@ -71,7 +84,7 @@ export default function AboutUsSection() {
         {/* image and contact */}
         <div className=" hidden w-1/2 xl:grid grid-cols-1 grid-rows-4 items-center justify-center">
           {/* image */}
-          <div className="col-span-1 row-span-3 rounded-2xl shadow-lg">
+          <div className="col-span-1 row-span-3 rounded-2xl shadow-lg motion-preset-expand motion-duration-1000">
             <Image
               src={Automacert}
               alt=""
@@ -80,7 +93,7 @@ export default function AboutUsSection() {
           </div>
 
           {/* contact */}
-          <div className="col-span-1 row-span-1 flex flex-col items-start justify-center gap-6">
+          <div className="col-span-1 row-span-1 flex flex-col items-start justify-center gap-6 lg:motion-preset-slide-left motion-duration-1500">
             <h4 className="text-xl leading-7 font-semibold text-slate-500">Saiba onde nos encontrar</h4>
 
             {/* cards-container */}
--- components/sections/about-us/stats-card.tsx
+++ components/sections/about-us/stats-card.tsx
@@ -1,12 +1,17 @@
+import { cn } from "@/lib/utils"
 
 interface Props {
   title: string
   description: string
+  className?: string
 }
 
-export default function StatsCard({title, description}: Props) {
+export default function StatsCard({ title, description, className }: Props) {
   return (
-    <div className='min-w-[280px] max-w-full min-h-[150px] bg-background text-primary flex flex-col items-start justify-center text-start py-4 px-6 shadow-lg rounded-2xl'>
+    <div className={cn(
+      'min-w-[280px] max-w-full min-h-[150px] bg-background text-primary flex flex-col items-start justify-center text-start py-4 px-6 shadow-lg rounded-2xl',
+      className
+    )}>
       <h1 className="text-4xl lg:text-5xl leading-13 lg:leading-14 font-bold">{title}</h1>
       <h2 className="text-base lg:text-lg leading-6 lg:leading-7 font-semibold">{description}</h2>
     </div>
