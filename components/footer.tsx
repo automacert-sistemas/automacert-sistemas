0843c910cc7afa4f0925515d9570ccc225f36b2a
VictorJGB <victorgb.dev@gmail.com>
2025-06-05 17:14:59 -0300

    feat: Aplicando mudanças no footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -18,18 +18,18 @@ export default function Footer() {
         {/* main content */}
         <div className="w-full grid grid-cols-1 grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 items-center lg:items-start justify-center gap-8">
           {/* image */}
-            <div className='w-52 lg:w-28 mx-auto lg:mx-0'>
-              <Link href={'/'}>
-                <Image src={Logo}  alt='' className='hidden lg:flex w-full object-contain'/>
-                <Image src={smLogo}  alt='' className='lg:hidden w-full object-contain'/>
-              </Link>
-            </div>
+          <div className='w-52 lg:w-28 mx-auto lg:mx-0'>
+            <Link href={'/'}>
+              <Image src={Logo} alt='' className='hidden lg:flex w-full object-contain' />
+              <Image src={smLogo} alt='' className='lg:hidden w-full object-contain' />
+            </Link>
+          </div>
 
           {/* links */}
           <div className='flex flex-col items-center justify-center lg:items-start gap-5'>
             <h4 className='min-sm:max-md:text-2xl text-xl text-slate-900 font-semibold'>Links</h4>
             <nav className='flex flex-col gap-2.5 items-center justify-center lg:items-start'>
-              {navLinks.map(({id, label}, index) => (
+              {navLinks.map(({ id, label }, index) => (
                 <Link key={index} href={id} className='min-sm:max-md:text-lg hover:text-primary text-slate-700'>
                   {label}
                 </Link>
@@ -44,13 +44,13 @@ export default function Footer() {
             <div className='w-full flex flex-col items-center justify-center lg:items-start lg:justify-start gap-2.5'>
               {/* address */}
               <div className='flex items-start justify-start gap-2'>
-                <MapPin className='min-sm:max-md:size-5 size-4 text-slate-900'/>
+                <MapPin className='min-sm:max-md:size-5 size-4 text-slate-900' />
                 <p className='min-sm:max-md:text-base text-sm text-slate-700'>R. Tristão Gonçalves, 573 - <br /> Centro, Crato - CE 63100-100</p>
               </div>
 
               {/* phones */}
               <div className='flex items-start justify-start gap-2'>
-                <Phone className='min-sm:max-md:size-5 size-4 text-slate-900'/>
+                <Phone className='min-sm:max-md:size-5 size-4 text-slate-900' />
                 <div className='flex flex-col items-start justify-start min-sm:max-md:text-base text-sm text-slate-700 gap-1'>
                   <p>Suporte: (88) 3523-3951</p>
                   <p>Certificado: (88) 3532-0974</p>
@@ -63,11 +63,11 @@ export default function Footer() {
           </div>
         </div>
 
-        <Separator className='w-2/3 lg:w:1/2 bg-slate-300'/>
+        <Separator className='w-2/3 lg:w:1/2 bg-slate-300' />
 
         {/* company info */}
-        <div className='flex flex-col items-center justify-center md:flex-row text-sm text-slate-500'>
-          <span>© 2025 ZUCCHETTI SOFTWARE E SISTEMAS LTDA.</span>
+        <div className='flex flex-col items-center justify-center text-center lg:flex-row gap-1 text-sm text-slate-500'>
+          <span>© 2025 AUTOMATEC AUTOMACAO COMERCIAL E TECNOLOGIA DA INFORMACAO LTDA.</span>
           <span>Todos os direitos reservados.</span>
           <span>CNPJ 23.785.312/0001-53.</span>
         </div>
